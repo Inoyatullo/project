7e3e8351a16008b2405c586f036333d8ec33ccc1
Inoyatullo <you@example.com>
2020-03-06 14:32:59 +0500

    refact: add scss

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ const autoprefixer = require('gulp-autoprefixer');
   });
   watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
@@ -27,7 +28,7 @@ function replace(done) {
 };
 
 function serveSass() {
-  return src("./sass/*.sass")
+  return src("./sass/**/*.sass","./sass/**/*.scss")
       .pipe(sass())
       .pipe(autoprefixer({
         cascade: false
