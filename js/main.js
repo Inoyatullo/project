f07b62496abb7bd9570f768a6af508d62a43af16
Inoyatullo <you@example.com>
2020-03-12 20:02:38 +0500

    refact: add to site scrollTop

--- js/main.js
+++ js/main.js
@@ -20,8 +20,8 @@
 $(document).ready(function () {
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
-      closeBtn = $('.modal__close');
-      window = $('body');
+      closeBtn = $('.modal__close'),
+      window = $('.modal__close');
 
   modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
@@ -29,5 +29,23 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
-  
+  /* window.on('click', function () {
+    modal.toggleClass('modal--visible');
+  }); */
+});
+
+$(document).ready(function () {
+  var btnTop = $('.btn-top');
+
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() >= 150 ) {
+      $(btnTop).fadeIn();
+    }else {
+      $(btnTop).fadeOut();
+    }
+  });
+
+  $(btnTop).on('click', function () {
+    $('html,body').animate({scrollTop:0}, 1000);
+  });
 });
